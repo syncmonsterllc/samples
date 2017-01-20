7625c17f18865ff7638317c1f391c3ac4a0b6dbd
Dissect Front-End Dev <papp.dany77@icloud.com>
2017-01-20 11:53:20 -0500

    Update config.sample.js

--- push-messaging-and-notifications/config.sample.js
+++ push-messaging-and-notifications/config.sample.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: '<Your Cloud Messaging API Key from https://console.firebase.google.com>'
+  gcmAPIKey: '<AAAAdQN2514:APA91bGxGI8FtRoKT7Tyi_qObjJrDtNTL4dMLDDDK5ai7NC6EUVigwbQA834R083D_dFYTdY78gyH-3qKRxX8HRTXUwmxJaiiA6U2v8Tx3wMBpJ5BQH5HN21eXYTocqIoA0CFgLRXpKj>'
 };
